36833de749defaa293f9e31445551d80b78b330d
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-04-27 15:48:43 -0400

    make legit move test, failed

--- src/test/player.test.js
+++ src/test/player.test.js
@@ -5,3 +5,10 @@ test('init player', () => {
     let p1 = player('Player 1', 'user')
     expect(p1).not.toBeNull()
 })
+
+test('make legit move', () => {
+    let p1 = player('Player 1', 'user')
+    let enemyBoard = gameboard()
+    p1.takeTurn("01", enemyBoard)
+    expect(enemyBoard.getMissedAttack()[0][1]).toEqual(true)
+})
