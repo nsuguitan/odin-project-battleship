3abb8bb37d1d151be0c7adb92fc0676d19499c30
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-05-05 15:23:06 -0400

    successfully add carrier to player board

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import { gsap } from "gsap";
 import { Draggable } from "gsap/Draggable";
+import "./factories/ship.js";
+import "./factories/player.js";
+import "./factories/gameboard.js";
+import gameboardFactory from "./factories/gameboard.js";
+import shipFactory from "./factories/ship.js";
+import { _colorStringFilter } from "gsap/gsap-core";
+
+
 gsap.registerPlugin( CSSPlugin )
 gsap.registerPlugin(Draggable);
 
@@ -73,7 +81,7 @@ $(document).ready(function(){
           gsap.to('#submarine', {transform: "rotate(90deg)"})
         }  
         });
-        let destroyerDrag = Draggable.create("#destroyer",{
+      let destroyerDrag = Draggable.create("#destroyer",{
           type: "x,y",
           liveSnap:{
               x: function(endValue) {
@@ -89,6 +97,7 @@ $(document).ready(function(){
               gsap.to('#destroyer', {transform: "rotate(90deg)"})
             }  
             });
+      document.getElementById("start-button").addEventListener("click", function(){secureShips()});  
     }, 300);
 
     function shipStartLocation(shipId){
@@ -106,10 +115,36 @@ $(document).ready(function(){
       console.log(shipId+ " y:"+ cy)
       console.log(" Grid (x,y): ("+ originX + ", " + originY + ")")
       console.log(shipId + " (x,y): ("+ gx + ", " + gy + ")")
+      //y-axis location is the row, x-axis location is column
+      return[gy,gx]
 
     }
 
     function secureShips(){
+      console.log("Start Button Clicked --- let's goooooo!")
+      //Human Player board generation
+      const myBoard = gameboardFactory()
+      const myCarrier = shipFactory(5)
+
+      let [cRow, cCol] = shipStartLocation("carrier")
+      console.log("coords:",[cRow, cCol])
+      let carrierTransformString = '' + document.getElementById("carrier").style.transform
+      console.log("Transform string: ", carrierTransformString)
+      let rotation = carrierTransformString.match(/rotate\(\d+deg\)/i);
+      console.log("carrier rotation: ", rotation)
+      if (rotation && rotation[0] === 'rotate(90deg)'){
+        myBoard.placeShip(myCarrier,cRow, cCol, true)
+        console.log("-------- Vert INFO------")
+        console.table(myBoard.getBoard())
+        console.table(myBoard.getShips())
+      }
+      else{
+        myBoard.placeShip(myCarrier,cRow, cCol, false)
+        console.log("-------- Horizontal INFO------")
+        console.table(myBoard.getBoard())
+        console.table(myBoard.getShips())
+      }
+      
 
     }
 
